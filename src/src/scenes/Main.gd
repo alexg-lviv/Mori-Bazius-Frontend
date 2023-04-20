117156d2a04cb28a91abd30463fd6f86c393ff05
oleksadobush <oleksandra.stasiuk@ucu.edu.ua>
2023-04-20 13:22:08 +0300

    add icons for tabs

--- src/src/scenes/Main.gd
+++ src/src/scenes/Main.gd
@@ -1,1 +1,21 @@
 extends Control
+
+var home_icon = preload("res://icon.svg")
+var leadboard_icon = preload("res://icon.svg")
+var guild_icon = preload("res://icon.svg")
+var inventory_icon = preload("res://icon.svg")
+var witchers_icon = preload("res://icon.svg")
+
+var num_tabs = 5
+
+@onready var TabsContainer = get_node("TabContainer")
+
+func _ready():
+	TabsContainer.set_tab_icon(0, home_icon)
+	TabsContainer.set_tab_icon(1, leadboard_icon)
+	TabsContainer.set_tab_icon(2, guild_icon)
+	TabsContainer.set_tab_icon(3, inventory_icon)
+	TabsContainer.set_tab_icon(4, witchers_icon)
+	
+	for i in range(num_tabs):
+		TabsContainer.set_tab_title(i, "")
