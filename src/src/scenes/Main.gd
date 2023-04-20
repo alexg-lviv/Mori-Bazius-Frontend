9feb4f1a49461cd09c88546c5687d8a8d14d691a
alortius <anna-alina.bondarets@ucu.edu.ua>
2023-04-20 16:40:54 +0300

    Fix

--- src/src/scenes/Main.gd
+++ src/src/scenes/Main.gd
@@ -4,9 +4,8 @@ var home_icon = preload("res://icon.svg")
 var leadboard_icon = preload("res://icon.svg")
 var guild_icon = preload("res://icon.svg")
 var inventory_icon = preload("res://icon.svg")
-var witchers_icon = preload("res://icon.svg")
 
-var num_tabs = 5
+var num_tabs = 4
 
 @onready var TabsContainer = get_node("TabContainer")
 
@@ -15,7 +14,6 @@ func _ready():
 	TabsContainer.set_tab_icon(1, leadboard_icon)
 	TabsContainer.set_tab_icon(2, guild_icon)
 	TabsContainer.set_tab_icon(3, inventory_icon)
-	TabsContainer.set_tab_icon(4, witchers_icon)
 	
 	for i in range(num_tabs):
 		TabsContainer.set_tab_title(i, "")
