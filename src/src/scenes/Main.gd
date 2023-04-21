bd247ef99b098119e3d90bb3fd29a45bbb9cc070
alortius <anna-alina.bondarets@ucu.edu.ua>
2023-04-21 18:43:39 +0300

    Change guild tab to monsters

--- src/src/scenes/Main.gd
+++ src/src/scenes/Main.gd
@@ -1,9 +1,9 @@
 extends Control
 
 var home_icon = preload("res://icon.svg")
-var leadboard_icon = preload("res://icon.svg")
-var guild_icon = preload("res://icon.svg")
 var inventory_icon = preload("res://icon.svg")
+var monsters_icon = preload("res://icon.svg")
+var leaderboard_icon = preload("res://icon.svg")
 
 var num_tabs = 4
 
@@ -12,9 +12,9 @@ var num_tabs = 4
 
 func _ready():
 	TabsContainer.set_tab_icon(0, home_icon)
-	TabsContainer.set_tab_icon(1, leadboard_icon)
-	TabsContainer.set_tab_icon(2, guild_icon)
-	TabsContainer.set_tab_icon(3, inventory_icon)
+	TabsContainer.set_tab_icon(1, inventory_icon)
+	TabsContainer.set_tab_icon(2, monsters_icon)
+	TabsContainer.set_tab_icon(3, leaderboard_icon)
 	
 	for i in range(num_tabs):
 		TabsContainer.set_tab_title(i, "")
