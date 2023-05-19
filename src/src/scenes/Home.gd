cb7f25a27f9bb46197f55bad74ba1cd982b64c6e
alortius <anna-alina.bondarets@ucu.edu.ua>
2023-05-20 00:53:04 +0300

    Don't you stray, don't fade away

--- src/src/scenes/Home.gd
+++ src/src/scenes/Home.gd
@@ -4,14 +4,14 @@ extends Control
 @onready var _tabs = get_node("TabContainer") as TabContainer
 @onready var _num_tabs: int = _tabs.get_child_count()
 
-const _icons := {
-	0: preload("res://icon.svg"),
-	1: preload("res://icon.svg"),
-	2: preload("res://icon.svg"),
-	3: preload("res://icon.svg"),
+const _textures := {
+	0: preload("res://art/UI/tabs/herbs_potions.png"),
+	1: preload("res://art/UI/tabs/minerals_metals.png"),
+	2: preload("res://art/UI/tabs/equipment.png"),
+	3: preload("res://art/UI/tabs/utility.png"),
 }
 
 func _ready():
 	for i in range(_num_tabs):
-		_tabs.set_tab_icon(i, _icons[i])
+		_tabs.set_tab_icon(i, _textures[i])
 		_tabs.set_tab_title(i, "")
