b0ffabc1679a2f90daa97b671e53859043573ced
alortius <anna-alina.bondarets@ucu.edu.ua>
2023-05-21 22:44:03 +0300

    Break down requests

--- src/src/scenes/UI/ErrorPopup.gd
+++ src/src/scenes/UI/ErrorPopup.gd
@@ -1,2 +1,10 @@
 extends Control
 
+@onready var _main_menu = preload("res://src/scenes/Login.tscn").instantiate()
+
+var error:
+	set(value):
+		get_node("TextureRect/VBoxContainer/Label").text = value
+
+func _on_texture_button_pressed():
+	get_tree().get_root().add_child(_main_menu)
