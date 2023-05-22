71e97c101b89c68043c82233f0fa854ad42d82a4
alortius <anna-alina.bondarets@ucu.edu.ua>
2023-05-22 19:10:21 +0300

    Add hunters and monsters animations

--- src/src/scenes/buttons/Monster.gd
+++ src/src/scenes/buttons/Monster.gd
@@ -10,10 +10,10 @@ extends Control
 			_master_p = Items.monsters[monster_name]["master_p"]
 			_exp = Items.monsters[monster_name]["exp"]
 			get_node("Description/Label").text = monster_name.capitalize()
-			get_node("Description/Stats/Exp/Label").text = str(_exp)
-			get_node("Description/Stats/Probs/VBoxContainer/Hunter/Label").text = str(_hunter_p)
-			get_node("Description/Stats/Probs/VBoxContainer/Master/Label").text = str(_master_p)
-			
+			get_node("Description/Exp/Label").text = str(_exp)
+			get_node("Hunter/WR/Label").text = str(100 * (1 - _hunter_p)) + "%"
+			get_node("Master/WR/Label").text = str(100 * (1 - _master_p)) + "%"
+
 
 var _hunter_p: float
 var _master_p: float
@@ -27,20 +27,30 @@ var _rot_tween_master
 @onready var _hunter = get_node("Hunter") as TextureButton
 @onready var _master = get_node("Master") as TextureButton
 
-@onready var _hunters_logs = get_node("HuntersLogs") as VBoxContainer
-@onready var _masters_logs = get_node("MastersLogs") as VBoxContainer
-
 @onready var _description = get_node("Description") as VBoxContainer
+
+@onready var _hunters_wr = get_node("Hunter/WR") as HBoxContainer
+@onready var _masters_wr = get_node("Master/WR") as HBoxContainer
 
 @onready var _spawn_hunters = get_node("Hunter/ParticlesSpawn") as Node2D
 @onready var _spawn_masters = get_node("Master/ParticlesSpawn") as Node2D
 
 @onready var _particles := preload("res://src/scenes/particles/Click.tscn")
 
+@onready var _hunter_death_anim = get_node("Hunter/AnimatedSprite2D")
+@onready var _master_death_anim = get_node("Master/AnimatedSprite2D")
+@onready var _monster_death_anim = get_node("Monster/AnimatedSprite2D")
 
 
 func _ready():
+	$Hunter/Smoke.visible = false
+	$Master/Smoke.visible = false
+	_hunters_wr.visible = false
+	_masters_wr.visible = false
 	_description.visible = false
+	_hunter_death_anim.visible = false
+	_master_death_anim.visible = false
+	_monster_death_anim.visible = false
 	Events.update_qty.connect(_validate)
 	Events.set_qty.connect(_val)
 	_validate("", 0)
@@ -53,48 +63,42 @@ func _val():
 	_master.disabled = true if Items.stats["masters"] == 0 else false
 	_monster.disabled = true if _hunter.disabled and _master.disabled else false
 
+func _play_death_anim(sprite: AnimatedSprite2D):
+	sprite.visible = true
+	sprite.play("default")
+	await sprite.animation_finished
+	sprite.visible = false
+
+func _play_smoke_anim(sprite: AnimatedSprite2D):
+	sprite.visible = true
+	sprite.play_backwards("default")
+	await sprite.animation_finished
+	sprite.visible = false
 
 func _on_hunter_pressed():
+	_play_smoke_anim($Hunter/Smoke)
 	_rotate(_hunter, _rot_tween_hunter)
-	_emit_particles(_spawn_hunters, Items.data["silver_sword"]["sprite"])
+	_emit_fight_particles(Items.data["silver_sword"]["sprite"])
 	
 	if randf_range(0, 1) <= _hunter_p:
-		_display_log(_hunters_logs, "\nHunter died fighting " + monster_name)
+		_play_death_anim(_hunter_death_anim)
 		Events.emit_signal("update_qty", "hunter", -1)
 	else:
-		_display_log(_hunters_logs, "\nHunter slayed " + monster_name)
+		_play_death_anim(_monster_death_anim)
 		Events.emit_signal("update_exp", _exp)
 
 
 func _on_master_pressed():
+	_play_smoke_anim($Master/Smoke)
 	_rotate(_master, _rot_tween_master)
-	_emit_particles(_spawn_masters, Items.data["kingslayers_silver_sword"]["sprite"])
+	_emit_fight_particles(Items.data["kingslayers_silver_sword"]["sprite"])
 	
 	if randf_range(0, 1) <= _master_p:
-		_display_log(_masters_logs, "\nMaster died fighting " + monster_name)
+		_play_death_anim(_master_death_anim)
 		Events.emit_signal("update_qty", "master", -1)
 	else:
-		_display_log(_masters_logs, "\nMaster slayed " + monster_name)
+		_play_death_anim(_monster_death_anim)
 		Events.emit_signal("update_exp", _exp)
-
-
-func _display_log(logs_container: VBoxContainer, line: String):
-	var timer := Timer.new()
-	var label := Label.new()
-	add_child(timer)
-	logs_container.add_child(label)
-	label.horizontal_alignment = HORIZONTAL_ALIGNMENT_RIGHT if logs_container == _hunters_logs else HORIZONTAL_ALIGNMENT_LEFT
-	timer.wait_time = 1.5
-	label.text = line
-	timer.timeout.connect(_delete_log.bind(timer, label))
-	timer.start()
-
-func _delete_log(timer: Timer, label: Label):
-	var tween = create_tween()
-	tween.tween_property(label, "modulate", Color.TRANSPARENT, 0.5)
-	tween.tween_callback(label.queue_free)
-	timer.queue_free()
-
 
 func _scale_up(butt: TextureButton):
 	var tween = get_tree().create_tween()
@@ -115,31 +119,49 @@ func _rotate(butt: TextureButton, tween):
 
 func _on_monster_mouse_entered():
 	_description.visible = true
+	_hunters_wr.visible = true
+	_masters_wr.visible = true
+	_scale_up(_hunter)
+	_scale_up(_master)
+	_scale_up(_monster)
 
 func _on_monster_mouse_exited():
 	_description.visible = false
+	_hunters_wr.visible = false
+	_masters_wr.visible = false
+	_scale_down(_hunter)
+	_scale_down(_master)
+	_scale_down(_monster)
 
 
 func _on_hunter_mouse_entered():
+	_description.visible = true
+	_hunters_wr.visible = true
 	_scale_up(_hunter)
 	_scale_up(_monster)
 
 func _on_hunter_mouse_exited():
+	_description.visible = false
+	_hunters_wr.visible = false
 	_scale_down(_hunter)
 	_scale_down(_monster)
 
 
 func _on_master_mouse_entered():
+	_description.visible = true
+	_masters_wr.visible = true
 	_scale_up(_master)
 	_scale_up(_monster)
 
 func _on_master_mouse_exited():
+	_description.visible = false
+	_masters_wr.visible = false
 	_scale_down(_master)
 	_scale_down(_monster)
 
 
-func _emit_particles(spawn_position, texture):
+func _emit_fight_particles(texture):
 	var p = _particles.instantiate()
-	spawn_position.add_child(p)
+	$Monster/ParticlesSpawn.add_child(p)
 	p.texture = texture
 	p.emit_and_free()
