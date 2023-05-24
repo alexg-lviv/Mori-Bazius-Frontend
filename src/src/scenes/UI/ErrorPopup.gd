2ab6d9592c45b63a61f45eb1082a168654013b60
alortius <anna-alina.bondarets@ucu.edu.ua>
2023-05-24 17:50:55 +0300

    Add popup entrence and exit animations

--- src/src/scenes/UI/ErrorPopup.gd
+++ src/src/scenes/UI/ErrorPopup.gd
@@ -4,9 +4,31 @@ extends Control
 
 var error:
 	set(value):
-		get_node("TextureRect/VBoxContainer/Label").text = value
+		get_node("TextureRect/Label").text = value
 
-func _on_texture_button_pressed():
+
+func _ready():
+	var tween = get_tree().create_tween().set_parallel().set_trans(Tween.TRANS_BACK).set_ease(Tween.EASE_OUT)
+	tween.tween_property($TextureRect, "scale", Vector2.ONE, 0.4).from(0.3 * Vector2.ONE)
+	tween.tween_property($TextureRect, "rotation_degrees", 0, 0.4).from(45)
+
+
+func _on_texture_button_pressed():	
+	var tween = get_tree().create_tween().set_parallel().set_trans(Tween.TRANS_BACK).set_ease(Tween.EASE_IN)
+	tween.tween_property($TextureRect, "scale", Vector2.ZERO, 0.4)
+	tween.tween_property($TextureRect, "rotation_degrees", 45, 0.4)
+	await tween.finished
+	
 	get_tree().get_root().add_child(_main_menu)
 	get_tree().get_root().get_node("Main").queue_free()
 	queue_free()
+
+
+func _on_texture_button_mouse_entered():
+	var tween = get_tree().create_tween()
+	tween.tween_property($TextureRect/TextureButton, "scale", 1.15 * Vector2.ONE, 0.05).set_trans(Tween.TRANS_LINEAR).set_ease(Tween.EASE_OUT)
+
+
+func _on_texture_button_mouse_exited():
+	var tween = get_tree().create_tween()
+	tween.tween_property($TextureRect/TextureButton, "scale", Vector2.ONE, 0.05).set_trans(Tween.TRANS_LINEAR).set_ease(Tween.EASE_OUT)
